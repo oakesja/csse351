6c40f984e30d21afca84b7cde5130d20988337ec
Jacob Oakes <jacoboakes92@gmail.com>
2014-10-14 12:02:33 -0400

    added better drawing

--- backgammon.js
+++ backgammon.js
@@ -9,9 +9,19 @@ var triangleWidth = .15;
 var triangleHeight = .8;
 var diePointRad = .015;
 var diePointNum = 8;
+var circlePoints = 20;
 var dice = [];
 var holders = [];
-var checkers = [];
+var squares = [[],[]];
+var circles = [[],[]];
+var triangles = [[], []];
+var vBuffer;
+var cBuffer;
+
+var black = vec4( 0.0, 0.0, 0.0, 1.0 );
+var white = vec4( 1.0, 1.0, 1.0, 1.0 );
+var brown = vec4( 0.59765625, 0.296875, 0.0, 1.0 )
+var red = vec4( 1.0, 0.0, 0.0, 1.0 );
 
 window.onload = function init()
 {
@@ -28,8 +38,8 @@ window.onload = function init()
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, 8*maxPoints, gl.STATIC_DRAW );  
 
     // Associate out shader variables with our data buffer
@@ -37,18 +47,24 @@ window.onload = function init()
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
+    cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, 16*maxPoints, gl.STATIC_DRAW );
+    
+    var vColor = gl.getAttribLocation( program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
     createHolders();
     drawBoard();
     createDice();
     drawDice();
 
-    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
 
     // click listener
     canvas.addEventListener ("click", function(event) {
         centerX = 2*(event.clientX-canvas.offsetLeft)/canvas.width-1;
         centerY = 2*(canvas.height-event.clientY+canvas.offsetTop)/canvas.height-1;
-        // drawPolygon(centerX, centerY, 20, checkerRadius);
         closet = 0;
         closetDist = 2;
         for(var i=0; i<holders.length; i++){
@@ -81,12 +97,44 @@ window.onload = function init()
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.LINES, 0, points.length);
+
+    //draws squares
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(squares[0]));
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(squares[1]));
+
+    for(var i=0; i< squares[0].length; i+=4){
+        gl.drawArrays( gl.TRIANGLE_FAN, i, 4 );
+    }
+
+    //draws triangles
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(triangles[0]));
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(triangles[1]));
+
+    for(var i=0; i< triangles[0].length; i+=3){
+        gl.drawArrays( gl.TRIANGLES, i, 3 );
+    }
+
+    //draws circles
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(circles[0]));
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(circles[1]));
+
+    for(var i=0; i< circles[0].length; i+=circlePoints){
+        gl.drawArrays( gl.TRIANGLE_FAN, i, circlePoints );
+    }
+
     requestAnimFrame (render);
 }
 
 function redraw(){
-    points = [];
+    circles = [[], []];
+    squares = [[], []];
+    triangles = [[], []];
     drawBoard();
     drawDice();
     gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
@@ -96,30 +144,20 @@ function randomBetween(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function drawPolygon(centerX, centerY, numPoints, radius){
+function drawCircle(centerX, centerY, radius, color){
     var theta=0;
-    var lastPoint = vec2(centerX + radius, centerY);
-    for(var i = 0; i < numPoints+1; i++){
-        points.push(lastPoint);
-        lastPoint = vec2(radius * Math.cos(theta) +centerX, radius * Math.sin(theta) + centerY); 
-        points.push(lastPoint);
-        theta+=2*Math.PI/numPoints;
+    for(var i = 0; i < circlePoints; i++){
+        point = vec2(radius * Math.cos(theta) +centerX, radius * Math.sin(theta) + centerY); 
+        circles[0].push(point);
+        circles[1].push(color);
+        theta+=2*Math.PI/circlePoints;
     }
 }
 
 function drawDice(){
     for(var i=0; i<dice.length; i++){
         var die = dice[i]
-        points.push(die.topLeft);
-        points.push(die.topRight);
-        points.push(die.topRight);
-        points.push(die.bottomRight);
-        points.push(die.bottomRight);
-        points.push(die.bottomLeft);
-        points.push(die.bottomLeft);
-        points.push(die.topLeft);
-        points.push(vec2(-0.3, 0.075))
-        points.push(vec2(-0.3, -0.075))
+        drawSquare(die.topLeft, die.topRight, die.bottomRight, die.bottomLeft, white);
         drawNumberOnDie(die.centerX, die.centerY, die.number);
     }
 }
@@ -140,24 +178,24 @@ function createDie(topLeftx, topLefty){
 }
 
 function drawNumberOnDie(dieCenterx, dieCentery, value){
-    
+    var color = black;
     if(value == 1 || value == 5 || value == 3){
-        drawPolygon(dieCenterx, dieCentery, diePointNum, diePointRad);
+        drawCircle(dieCenterx, dieCentery, diePointRad, color);
     }
     
     if(value != 1){
-        drawPolygon(dieCenterx-triangleWidth/4, dieCentery+triangleWidth/4, diePointNum, diePointRad);
-        drawPolygon(dieCenterx+triangleWidth/4, dieCentery-triangleWidth/4, diePointNum, diePointRad);
+        drawCircle(dieCenterx-triangleWidth/4, dieCentery+triangleWidth/4, diePointRad, color);
+        drawCircle(dieCenterx+triangleWidth/4, dieCentery-triangleWidth/4, diePointRad, color);
     }
     
     if(value != 1 && value!=2 && value!=3){
-        drawPolygon(dieCenterx+triangleWidth/4, dieCentery+triangleWidth/4, diePointNum, diePointRad);
-        drawPolygon(dieCenterx-triangleWidth/4, dieCentery-triangleWidth/4, diePointNum, diePointRad);
+        drawCircle(dieCenterx+triangleWidth/4, dieCentery+triangleWidth/4, diePointRad, color);
+        drawCircle(dieCenterx-triangleWidth/4, dieCentery-triangleWidth/4, diePointRad, color);
     }
     
     if(value == 6){
-        drawPolygon(dieCenterx+triangleWidth/4, dieCentery, diePointNum, diePointRad);
-        drawPolygon(dieCenterx-triangleWidth/4, dieCentery, diePointNum, diePointRad);
+        drawCircle(dieCenterx+triangleWidth/4, dieCentery, diePointRad, color);
+        drawCircle(dieCenterx-triangleWidth/4, dieCentery, diePointRad, color);
     }
 }
 
@@ -178,14 +216,14 @@ function drawBoard(){
     var topRight = vec2(1, 1);
     var bottomRight = vec2(1, -1);
     var bottomLeft = vec2(-1, -1);
-    drawSquare(topLeft, topRight, bottomRight, bottomLeft);
+    drawSquare(topLeft, topRight, bottomRight, bottomLeft, brown);
 
     // draw middle
     topLeft = vec2(-triangleWidth/2, 1);
     topRight = vec2(triangleWidth/2, 1);
     bottomRight = vec2(triangleWidth/2, -1);
     bottomLeft = vec2(-triangleWidth/2, -1);
-    drawSquare(topLeft, topRight, bottomRight, bottomLeft);
+    drawSquare(topLeft, topRight, bottomRight, bottomLeft, black);
     
 }
 
@@ -193,12 +231,8 @@ function drawHolders(){
     var i;
     for(i=0; i<holders.length; i++){
         holder = holders[i];
-        points.push(holder.middle);
-        points.push(holder.left);
-        points.push(holder.left);
-        points.push(holder.right);
-        points.push(holder.right);
-        points.push(holder.middle);
+        triangles[0].push(holder.left, holder.right, holder.middle);
+        triangles[1].push(red, red, red);
         drawCheckers(holder);
     }
 }
@@ -212,19 +246,13 @@ function drawCheckers(holder){
         } else {
             y = holder.left[1] - checkerRadius- 2*checkerRadius*j;
         }
-        drawPolygon(x, y, 20, checkerRadius);
+        drawCircle(x, y, checkerRadius, white);
     }
 }
 
-function drawSquare(topLeft, topRight, bottomRight, bottomLeft){
-    points.push(topLeft);
-    points.push(topRight);
-    points.push(topRight);
-    points.push(bottomRight);
-    points.push(bottomRight);
-    points.push(bottomLeft);
-    points.push(bottomLeft);
-    points.push(topLeft);
+function drawSquare(topLeft, topRight, bottomRight, bottomLeft, color){
+    squares[0].push(topLeft, topRight, bottomRight, bottomLeft);
+    squares[1].push(color, color, color, color);
 }
 
 function createHolder(upLeft, flip){
